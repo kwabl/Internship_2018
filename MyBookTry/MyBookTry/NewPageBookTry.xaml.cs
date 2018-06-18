f911696462be0cf23072ec76299708bab5101395
Andrews <andrews.selasie@gmail.com>
2018-06-18 09:28:50 +0000

    Sqlite path

---
+++ MyBookTry/MyBookTry/NewPageBookTry.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyBookTry
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewPageBookTry : ContentPage
+	{
+		public NewPageBookTry ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Success", "We have handled the click event", "Great!");
+        }
+    }
+}
